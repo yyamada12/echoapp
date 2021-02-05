847e2178429f116cfaaf2549350ea8219c4316b4
yyamada12 <12yacropolisy@gmail.com>
2021-02-06 07:30:35 +0900

    feat: input view, controller

---
+++ src/main/java/example/app/EchoController.java
@@ -0,0 +1,17 @@
+package example.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("echo")
+public class EchoController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String viewInput(Model model) {
+        EchoForm form = new EchoForm();
+        model.addAttribute(form);
+        return "echo/input";
+    }
+}
---
+++ src/main/webapp/WEB-INF/echo/input.jsp
@@ -0,0 +1,14 @@
+<html>
+<body>
+    <h2>入力画面</h2>
+<form:form modelAttribute="echoForm">
+    <div>テキストを入力してください :</div>
+    <div>
+        <form:input path="text" />
+    </div>
+    <div>
+        <form:button>送信</form:button>
+    </div>
+</form:form>
+</body>
+</html>
