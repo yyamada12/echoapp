6911a11e9c89e6dc3f57daee0adb9911d286d5f9
yyamada12 <12yacropolisy@gmail.com>
2021-02-05 22:52:48 +0900

    initial commit

---
+++ src/main/java/example/config/AppConfig.java
@@ -0,0 +1,6 @@
+package example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {}
---
+++ src/main/java/example/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("example.app")
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registory) {
+        registory.jsp();
+    }
+}
